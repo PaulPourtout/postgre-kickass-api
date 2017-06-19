7e89a9d7f5894647c758a5f9f851d28645f94234
PaulPourtout <paulpourtout@hotmail.com>
2017-06-19 17:06:38 +0200

    added db

--- index.js
+++ index.js
@@ -2,9 +2,29 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+
 app.get('/', (req, res) => {
 	res.send('hello');
 });
+
+
+// DATABASE
+const pg = require('pg');
+
+app.get('/db', function (request, response) {
+	pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+		client.query('SELECT * FROM test_table', function (err, result) {
+			done();
+			if (err)
+			{ console.error(err); response.send("Error " + err); }
+			else
+			{ response.render('pages/db', { results: result.rows }); }
+		});
+	});
+});
+
+
+
 
 const apiRouter = express();
 
