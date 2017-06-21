1be1ec081ffb739a5b36ddd775d6bce3f11d9dc9
PaulPourtout <paulpourtout@hotmail.com>
2017-06-21 14:46:58 +0200

    changed middleware

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
 const apiRouter = express();
 
 // Set headers for responses
-apiRouter.use((req, res, next) => {
+app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, HEAD, PATCH");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
