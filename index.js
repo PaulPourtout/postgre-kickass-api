8927612e53540b5cd949894c7af1e3b5e1aa6e84
PaulPourtout <paulpourtout@hotmail.com>
2017-06-21 15:08:21 +0200

    changed middleware

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const apiRouter = express();
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, HEAD, PATCH");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Headers", "Origin, Access-Control-Allow-Origin, X-Requested-With, Content-Type, Accept");
 	res.header("Access-Control-Max-Age", "3600");
 	next();
 });
